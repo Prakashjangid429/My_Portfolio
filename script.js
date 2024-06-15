98022caa034335270f42c5da2243ce1820a72676
Prakashjangid429 <157800296+Prakashjangid429@users.noreply.github.com>
2024-06-15 19:17:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ function toggle(){
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2000,
+    duration: 500,
     delay: 100,
 
 });
 
 sr.reveal('.home-content h3,.home-content h1,.wrapper ,.home-content p,.heading,.project,.contact',{}); 
-sr.reveal('.service-content i,.service-content h3,.service-content p,.service-content .btn',{interval: 300}); 
-sr.reveal('.about-content h2,.about-content h4,.about-content p,.about-content .btn,.contact-input',{ interval: 300}); 
+sr.reveal('.service-content i,.service-content h3,.service-content p,.service-content .btn',{interval: 200}); 
+sr.reveal('.about-content h2,.about-content h4,.about-content p,.about-content .btn,.contact-input',{ interval: 200}); 
 
 
 const srbottom = ScrollReveal({
     origin: 'bottom',
     distance: '60px',
-    duration: 2000,
+    duration: 500,
     delay: 100,
 
 });
 srbottom.reveal('.logo,.menu,.icon,.btn,.home-img img',{});
 srbottom.reveal('.about-img',{delay:300})
-srbottom.reveal('.service-content,.project-content',{interval:300})
+srbottom.reveal('.service-content,.project-content',{interval:200})
